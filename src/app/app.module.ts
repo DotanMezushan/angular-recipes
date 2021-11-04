4964b774a250418c0f997eccad4f55833bf732e5
dotan mezushan <dotanbm3052@gmail.com>
2021-11-05 01:11:43 +0200

    massage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RecipeService } from './recipes/recipe.service';
-
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { RecipesRoutingModule } from './recipes/recipes-routing.module';
 import { ShoppingListModule } from './shopping-list/shopping-list-module';
 import { SharedModule } from './shared/shared.module';
+import { coreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -33,12 +27,12 @@ import { SharedModule } from './shared/shared.module';
     RecipesModule,
     RecipesRoutingModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    coreModule
     
     
   ],
-  providers: [ShoppingListService, RecipeService,{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService , multi: true}],
-
+  
 
   bootstrap: [AppComponent]
   
